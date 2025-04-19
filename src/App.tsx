bef08c5d1f19c0e4d6b7c15140955bfee4689b40
0nlyssslatts <sid36205@mail.ru>
2025-04-19 12:23:56 +0300

    Настрили репу

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
-import Button from '@components/Button'
+import "./App.css";
+import Button from "@components/ui/Button";
 
 function App() {
-  return (
-    <>
-      <div>
-        <Button>Мгу крутые</Button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <Button>Мгу крутые</Button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
